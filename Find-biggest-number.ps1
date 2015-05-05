125c42816613800f3e40401c090b6af82c2383dd
zleppy <zleppym@gmail.com>
2015-05-05 08:38:49 +0200

    PS_Find_biggest_number
    This PS script will find biggest number in an array of numbers

---
+++ Find-biggest-number.ps1
@@ -0,0 +1,20 @@
+﻿<#Variables #>
+Set-Variable $numbers
+Set-Variable $highest
+
+<#Set data in variables #>
+
+[int[]]$numbers = 10,20,35,74,41,80,92
+$highest = $numbers[0]
+
+<#Foreach loop to find highest number #>
+
+foreach ($int in $numbers)
+{
+    if ($int -gt $highest)
+        {
+            $highest = $int
+    }
+}
+
+$highest
